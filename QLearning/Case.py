fc42583279c8b669cc63ec8f43512c83ad966449
demo <demo@LAPTOP-1FTAQ0PU>
2018-02-06 17:39:25 +0100

    Case et type

---
+++ QLearning/Case.py
@@ -0,0 +1,6 @@
+from Type import Type
+class Case:  
+   def __init__(self, type, posX, posY):
+       self.type = type
+       self.x = posX
+       self.y = posY
---
+++ QLearning/Type.py
@@ -0,0 +1,8 @@
+from enum import Enum
+class Type(Enum):
+    Entree=1
+    Mur=2
+    Libre=3
+    Sortie=4
+    Piege=5
+    
