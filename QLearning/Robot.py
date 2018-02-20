a5bf7a9cc963079f6474ecc2918a18b84f22fbff
demo <demo@LAPTOP-1FTAQ0PU>
2018-02-20 16:35:42 +0100

    c

--- QLearning/Robot.py
+++ QLearning/Robot.py
@@ -8,6 +8,12 @@ class Robot:
     def __init__(self,labyrinthe, max_deplacement):
         self.lab = labyrinthe
         self.case = Case(Type.Entree, self.lab.depart[0], self.lab.depart[2])
-        self.penalite = 0
-        self.nb_deplacement_max = max_deplacement 
+        self.penalite = 0.99 #epsilon
+        self.gamma = 0.95
+        self.nb_deplacement_max = max_deplacement
+        self.Q #Q(s,a)
+        
+    def exploration(self):
+        self.lab.deplacement_possible(self.case)
+        
     
