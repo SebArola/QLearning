e93ec7b98f55590183826fd26f67849cc1bdda93
demo <demo@LAPTOP-1FTAQ0PU>
2018-02-20 16:56:08 +0100

    Case

--- QLearning/Case.py
+++ QLearning/Case.py
@@ -1,6 +1,9 @@
 from Type import Type
 class Case:  
-   def __init__(self, type, posX, posY):
+    def __init__(self, type, posX, posY):
        self.type = type
        self.x = posX
        self.y = posY
+    
+    def equal(self, case):
+        return self.x == case.x and self.y == case.y 
